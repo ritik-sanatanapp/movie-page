e79a3ed40dc10bcbc2c61c4a669fa36fe6eacdde
Ritik <ritik@sanatanapp.in>
2026-05-04 17:39:48 +0530

    using_mongodb

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
 import os
 import json
 
 app = Flask(__name__)
 MOVIES_DIR = "movies"
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mydb"]
+movies_collection  = db["movies"]
+
 
 # ------------------ LOAD COMMON ------------------
 def load_common():
@@ -11,13 +17,28 @@ def load_common():
         return json.load(f)
 
 # ------------------ LOAD MOVIE -------------------
+# def load_movie(movie_id, lang="en"):
+#     """Load movie JSON for given ID and language"""
+#     path = f'{MOVIES_DIR}/{movie_id}/data/{lang}.json'
+#     if not os.path.exists(path):
+#         path = f'{MOVIES_DIR}/{movie_id}/data/en.json' 
+#     with open(path, encoding="utf-8") as f:
+#         return json.load(f)
+
 def load_movie(movie_id, lang="en"):
-    """Load movie JSON for given ID and language"""
-    path = f'{MOVIES_DIR}/{movie_id}/data/{lang}.json'
-    if not os.path.exists(path):
-        path = f'{MOVIES_DIR}/{movie_id}/data/en.json'  # fallback to English
-    with open(path, encoding="utf-8") as f:
-        return json.load(f)
+    movie = movies_collection.find_one(
+        {"id": movie_id},
+        {"_id": 0}
+    )
+
+    if not movie:
+        return None
+
+    # optional: language handling (if you store translations)
+    if "lang" in movie and lang in movie["lang"]:
+        return movie["lang"][lang]
+
+    return movie
 
 # ------------------ LANGUAGE ---------------------
 def get_lang():
